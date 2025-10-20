6bc9ca6c38b26e2f8ab278bf7ea8c0cc955e736f
古川颯太 <sota_furukawa@icloud.com>
2025-10-20 21:25:23 +0900

    solve: agp4b ex1 and ex5, started C++ practice

---
+++ apg4b/ex5/main.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+  int a;
+  int b;
+  cin >> a;
+  cin >> b;
+  cout << a + b << endl;
+}
+
+/*初めてのC++プログラミング。
+環境構築から実際の問題の投稿までできてよかった。
+EX1とこれ（EX5）のみしか行っていないため、引き続きAPG4Bを進めていく。
+*/
